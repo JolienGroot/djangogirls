913a44cd9a8f6684ed272e7abfdc4cb58161cec8
Jolien <joliengroot@live.nl>
2018-02-03 13:54:00 +0100

    first commit of djangogirls yay

---
+++ djangogirls.py
@@ -0,0 +1,6 @@
+people = {'daniel': 37, 'jolien': 28, 'karis': 22}
+if people['karis']> people['jolien']:
+    print("Karis is Older than Jolien")
+else:
+    print("Karis is not older than Jolien")
+    print("Jolien is maybe the same age or older")
